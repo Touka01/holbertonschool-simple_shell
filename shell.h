83bc7142efb00cc146eb9528d35f39bd77548f2a
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-04-28 00:42:04 +0000

    SHELL

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef COMMAND_EXEC_H
-#define COMMAND_EXEC_H
+#ifndef _SHELL_H_\n#define SHELL_H
+
 
 #include <unistd.h>
 #include <stdio.h>
@@ -7,10 +7,22 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+
 #define MAX_LEN 1024
 
-void prompt();
-char* parse_input(char* buffer);
-void execute_command(char* command);
 
-#endif /* COMMAND_EXEC_H */
+extern char **environ;
+
+
+/* Display the prompt and wait for user input */
+void prompt(void);
+
+
+/* Parses the user input to get the command */
+char *parse_input(char *buffer);
+
+
+/* Execute the command entered by the user */
+void execute_command(char *command);
+
+#endif /* SHELL_H */
